7a8adaa80d1ae2e69e438eb0a4a1354c98844cde
divukman <divukman@gmail.com>
2020-01-27 13:12:42 +0100

    Refactoring

--- src/main/java/com/dimitar/msscbeerservice/domain/BeerOrderStatusEnum.java
+++
@@ -1,9 +0,0 @@
-package com.dimitar.msscbeerservice.domain;
-
-public enum BeerOrderStatusEnum {
-    NEW,
-    VALIDATED, VALIDATION_EXCEPTION,
-    ALLOCATED, ALLOCATION_EXCEPTION, PENDING_INVENTORY,
-    PICKED_UP,
-    DELIVERED, DELIVERY_EXCEPTION
-}
